c3df49652d488fb212098d9908b78445702a4a4f
hunbelievable <rustyholiday@gmail.com>
2015-12-08 14:24:13 -0600

    big change

--- js/root.js
+++ js/root.js
@@ -1,1 +1,10 @@
 // JavaScript source code
+angular.module("root", [])
+	.controller("index", ["$scope", function ($scope) {
+	    $scope.products = [
+			{ id: 1, name: "Hockey puck" },
+			{ id: 2, name: "Golf club" },
+			{ id: 3, name: "Baseball bat" },
+			{ id: 4, name: "Lacrosse stick" }
+	    ];
+	}]);
